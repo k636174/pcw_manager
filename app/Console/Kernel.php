acdfa3ccca457da49f65f9963fb956961dfee92f
shinji kaneko <shinji@k636174.net>
2019-03-09 23:25:56 +0900

    add cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+	Commands\StatusCheck::class
         //
     ];
 
@@ -26,6 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+	$schedule->command('status_check')->everyMinute();
     }
 
     /**
