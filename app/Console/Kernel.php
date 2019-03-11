97f5ff4f6587b69e14d96fb565a11cd4f446da1b
shinji kaneko <shinji@k636174.net>
2019-03-11 20:54:32 +0900

    change cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-	$schedule->command('status_check')->everyMinute()->withoutOverlapping();
+	$schedule->command('status_check')->everyMinute();
     }
 
     /**
