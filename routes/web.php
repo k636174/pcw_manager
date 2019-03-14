8862e8143fc54450329e43d86631b362595fcb1e
Shinji Kaneko <shinji@k636174.net>
2019-03-14 22:04:02 +0900

    change top_page

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HostController@index');
 
 Route::resource('groups', 'GroupController');
 Route::resource('hosts', 'HostController');
