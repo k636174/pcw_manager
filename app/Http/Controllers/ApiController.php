2d9bd86750914e8d66ffd04d8f38bd7dd2e80063
Shinji Kaneko <shinji@k636174.net>
2019-03-17 10:00:17 +0900

    change ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -34,7 +34,7 @@ class ApiController extends Controller
         $sql = "INSERT INTO `host_status` (host_id, status, lastcheck_at, created_at) VALUES ('$host_id','Active','$datetime','$datetime') ON DUPLICATE KEY UPDATE lastcheck_at = VALUES (lastcheck_at),status = 'Active';";
         \DB::statement($sql);
 
-        $sql = "INSERT INTO `group_host` (`host_id`, `group_id`) VALUES ('$host_id', '0') ON DUPLICATE KEY UPDATE host_id = VALUES ($host_id);";
+        $sql = "INSERT INTO `group_host` (`host_id`, `group_id`) VALUES ('$host_id', '0');";
         \DB::statement($sql);
 
         $sql = "INSERT INTO `host_ips` (host_id, src_gip, src_lip, created_at) VALUES ('$host_id','$src_gip','$src_lip','$datetime') ON DUPLICATE KEY UPDATE src_gip = '$src_gip',src_lip =  '$src_lip';";
