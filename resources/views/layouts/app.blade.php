9374ffe7e2480c273ec0529d11df5575f3567a4e
Shinji Kaneko <shinji@k636174.net>
2019-03-22 21:00:20 +0900

    change app.blade.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,9 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/groups">グループ</a>
+                        </li>
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
